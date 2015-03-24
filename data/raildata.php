0c344d631227b33b61e134646b70cbbdaa77c9dd
MartijnVerkleij <mail@mverkleij.nl>
2015-03-24 17:56:32 +0100

    first php

---
+++ data/raildata.php
@@ -0,0 +1,29 @@
+<?php
+	$stationdata = 
+		[
+				['Amf','Amersfoort',6],
+				['Brn','Baarn',4],
+				['Dld','Den Dolder',2],
+				['Stz','Soest-Zuid',1],
+				['St','Soest',2],
+				['Amfs','Amersfoort Schothorst',0],
+				['Sd','Soestdijk',1],
+				['Hvs','Hilversum',0],
+				['Bhv','Bilthoven',0]
+		];
+	$trackdata = 
+		[
+			['Amf','Brn'],
+			['Amf','Amfs'],
+			['Amf','Dld'],
+			['Sd','Brn'],
+			['Hlv','Brn'],
+			['Sd','St'],
+			['Stz','St'],
+			['Stz','Dld'],
+			['Dld','Bhv'],
+		];
+	
+	
+	
+	
---
+++ test.php
@@ -0,0 +1,53 @@
+<?php
+	require 'station.php';
+	require 'track.php' ;
+	
+	echo new DutchRailwayGraph();
+
+	class DutchRailwayGraph
+	{
+		public $stations;
+		public $tracks;
+		
+		public function __construct() {
+			
+			include 'data/raildata.php';
+			
+			foreach ($stationdata as $station) {
+				$this->addStation($station[0], $station[1]);
+			}
+			
+			foreach ($trackdata as $track) {
+				$this->addTrack($track[0], $track[1]);
+			}
+		}
+		
+		public function addStation($acr, $name) {
+			$this->$stations[$acr] = new station($acr, $name);
+		}
+		public function addTrack($station1, $station2) {
+			$this->$tracks[] = new track($station1, $station2);
+		}
+
+		public function getType($station) {
+			switch ($this->edgeCount($station)) {
+				case 1: return "end"; break;
+				case 2: return "normal"; break;
+				default: return "transit"; break;
+			}
+		}
+		
+		private function edgeCount($station) {
+			$i = 0;
+			foreach ($tracks as $track) {
+				if ($track->station1 == $station || $track->station2 == $station) {
+					$i++;
+				}
+			}
+			return $i;
+		}
+		
+		
+	}
+
+?>
